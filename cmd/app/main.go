a9fb02fa5a66e7e95e5cc23d7eb92d48dc6b6693
shortyk <shortykofficial@gmail.com>
2025-08-15 21:02:36 +0300

    feat: updated main.go

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"os/signal"
+	"syscall"
+
+	"github.com/shrtyk/avito-backend-spring-2025/internal/adapters/driven/tokens"
+	"github.com/shrtyk/avito-backend-spring-2025/pkg/config"
+	"github.com/shrtyk/avito-backend-spring-2025/pkg/logger"
+)
 
 func main() {
-	fmt.Println("hey")
+	cfg := config.MustInitConfig()
+	log := logger.MustCreateNewLogger(cfg.AppCfg.Env)
+	tService := tokens.MustCreateTokenService(&cfg.AuthTokenCfg)
+
+	app := NewApplication()
+	app.Init(
+		WithConfig(cfg),
+		WithLogger(log),
+		WithTokenService(tService),
+	)
+
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
+	defer cancel()
+
+	app.Serve(ctx)
 }
