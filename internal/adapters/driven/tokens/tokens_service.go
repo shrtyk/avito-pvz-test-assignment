cd0c295885f5ee54274f7ab88ba18efa529332c9
shortyk <shortykofficial@gmail.com>
2025-08-15 04:47:15 +0300

    feat: added token service implementation

---
+++ internal/adapters/driven/tokens/tokens_service.go
@@ -0,0 +1,96 @@
+package tokens
+
+import (
+	"crypto/rsa"
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"github.com/shrtyk/avito-backend-spring-2025/internal/core/domain/auth"
+	"github.com/shrtyk/avito-backend-spring-2025/pkg/config"
+)
+
+var (
+	ErrJWTValidation = errors.New("invalid token")
+	ErrJWTExpired    = errors.New("jwt expired")
+)
+
+type tokensService struct {
+	publicKey           *rsa.PublicKey
+	privateKey          *rsa.PrivateKey
+	accessTokenLifetime time.Duration
+}
+
+func MustCreateTokenService(cfg *config.AuthTokensCfg) *tokensService {
+	pubKeyData, err := os.ReadFile(cfg.PublicRSAPath)
+	if err != nil {
+		msg := fmt.Sprintf("failed to read: %s: %s", cfg.PublicRSAPath, err)
+		panic(msg)
+	}
+
+	pub, err := jwt.ParseRSAPublicKeyFromPEM(pubKeyData)
+	if err != nil {
+		msg := fmt.Sprintf("failed to parse public key: %s", err)
+		panic(msg)
+	}
+
+	privateKeyData, err := os.ReadFile(cfg.PrivateRSAPath)
+	if err != nil {
+		msg := fmt.Sprintf("failed to read: %s: %s", cfg.PrivateRSAPath, err)
+		panic(msg)
+	}
+
+	private, err := jwt.ParseRSAPrivateKeyFromPEM(privateKeyData)
+	if err != nil {
+		msg := fmt.Sprintf("failed to parse private key: %s", err)
+		panic(msg)
+	}
+
+	return &tokensService{
+		publicKey:           pub,
+		privateKey:          private,
+		accessTokenLifetime: cfg.JWTLifetime,
+	}
+}
+
+func (s *tokensService) GenerateAccessToken(tokenData auth.AccessTokenData) (string, error) {
+	claims := auth.AccessTokenClaims{
+		RegisteredClaims: jwt.RegisteredClaims{
+			Subject:   strconv.FormatInt(tokenData.UserID, 10),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(s.accessTokenLifetime)),
+			ID:        uuid.NewString(),
+		},
+		Role: string(tokenData.Role),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodRS256, claims)
+	return token.SignedString(s.privateKey)
+}
+
+func (s *tokensService) GetTokenClaims(token string) (*auth.AccessTokenClaims, error) {
+	tokenClaims := new(auth.AccessTokenClaims)
+	t, err := jwt.ParseWithClaims(token, tokenClaims, func(t *jwt.Token) (any, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodRSA); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
+		}
+		return s.publicKey, nil
+	})
+	if err != nil {
+		switch {
+		case errors.Is(err, jwt.ErrTokenExpired):
+			return nil, ErrJWTExpired
+		default:
+			return nil, ErrJWTValidation
+		}
+	}
+
+	if !t.Valid {
+		return nil, ErrJWTValidation
+	}
+
+	return tokenClaims, nil
+}
