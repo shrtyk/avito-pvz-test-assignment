79f56325eba4737aa4cb9d36211945dbdf316509
shortyk <shortykofficial@gmail.com>
2025-08-15 04:46:30 +0300

    feat: added interfaces for token service and empty (yet) repository interface

---
+++ internal/core/ports/auth.go
@@ -0,0 +1,8 @@
+package ports
+
+import "github.com/shrtyk/avito-backend-spring-2025/internal/core/domain/auth"
+
+type TokensService interface {
+	GenerateAccessToken(tokenData auth.AccessTokenData) (string, error)
+	GetTokenClaims(token string) (*auth.AccessTokenClaims, error)
+}
