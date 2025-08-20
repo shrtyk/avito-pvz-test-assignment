6728f377328a1f4d1d7b7877c4c8a2352babac16
shortyk <shortykofficial@gmail.com>
2025-08-20 05:28:08 +0300

    tests: added app handler test

---
+++ internal/adapters/driving/http/app_handler_test.go
@@ -0,0 +1,65 @@
+package http
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/shrtyk/avito-backend-spring-2025/pkg/logger"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHandle(t *testing.T) {
+	t.Parallel()
+
+	t.Run("success", func(t *testing.T) {
+		t.Parallel()
+		h := func(w http.ResponseWriter, r *http.Request) error {
+			w.WriteHeader(http.StatusOK)
+			return nil
+		}
+
+		server := httptest.NewServer(Handle(h))
+		defer server.Close()
+
+		resp, err := http.Get(server.URL)
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusOK, resp.StatusCode)
+	})
+
+	t.Run("http error", func(t *testing.T) {
+		t.Parallel()
+		h := Handle(func(w http.ResponseWriter, r *http.Request) error {
+			return &HTTPError{
+				Code:    http.StatusBadRequest,
+				Message: "Bad request",
+				Err:     errors.New("test error"),
+			}
+		})
+
+		server := httptest.NewServer(h)
+		defer server.Close()
+
+		resp, err := http.Get(server.URL)
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
+	})
+
+	t.Run("default error", func(t *testing.T) {
+		t.Parallel()
+		h := Handle(func(w http.ResponseWriter, r *http.Request) error {
+			return errors.New("test error")
+		})
+
+		l, _ := logger.NewTestLogger()
+		r := httptest.NewRequest(http.MethodGet, "/", nil)
+		r = r.WithContext(logger.ToCtx(context.Background(), l))
+
+		w := httptest.NewRecorder()
+		h(w, r)
+
+		assert.Equal(t, http.StatusInternalServerError, w.Code)
+	})
+}
