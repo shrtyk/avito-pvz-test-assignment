c248979d1098a60f9a743846ff4731ab5901b985
shortyk <shortykofficial@gmail.com>
2025-08-21 23:28:28 +0300

    feat: added initial grpc server

--- cmd/app/serve.go
+++ cmd/app/serve.go
@@ -5,13 +5,17 @@ import (
 	"errors"
 	"log/slog"
 	"net/http"
+	"sync"
 
+	"github.com/shrtyk/avito-pvz-test-assignment/internal/adapters/driving/grpc"
 	appHttp "github.com/shrtyk/avito-pvz-test-assignment/internal/adapters/driving/http"
 	"github.com/shrtyk/avito-pvz-test-assignment/pkg/logger"
 )
 
 func (app Application) Serve(ctx context.Context) {
-	s := http.Server{
+	var wg sync.WaitGroup
+
+	httpServ := http.Server{
 		Addr:         ":" + app.Cfg.HttpServerCfg.Port,
 		Handler:      appHttp.NewRouter(app.AppService, app.TokenService, app.Logger),
 		IdleTimeout:  app.Cfg.HttpServerCfg.IdleTimeout,
@@ -19,6 +23,7 @@ func (app Application) Serve(ctx context.Context) {
 		ReadTimeout:  app.Cfg.HttpServerCfg.ReadTimeout,
 		ErrorLog:     slog.NewLogLogger(app.Logger.Handler(), slog.LevelError),
 	}
+	grpcServ := grpc.NewGRPCServer(&wg, app.AppService, app.Logger, app.Cfg.GrpcServerCfg.Port)
 
 	eChan := make(chan error, 1)
 	go func() {
@@ -27,19 +32,28 @@ func (app Application) Serve(ctx context.Context) {
 		tCtx, tCancel := context.WithTimeout(context.Background(), app.Cfg.AppCfg.ShutdownTimeout)
 		defer tCancel()
 
-		eChan <- s.Shutdown(tCtx)
+		eChan <- grpcServ.Shutdown(tCtx)
+		eChan <- httpServ.Shutdown(tCtx)
 	}()
 
 	app.Logger.Info(
-		"Application successfully started",
+		"GRPC server successfully started",
+		slog.String("address", ":"+app.Cfg.GrpcServerCfg.Port),
+	)
+	grpcServ.MustStart()
+
+	app.Logger.Info(
+		"HTTP server successfully started",
 		slog.String("address", ":"+app.Cfg.HttpServerCfg.Port),
 	)
-	if err := s.ListenAndServe(); err != nil {
+	if err := httpServ.ListenAndServe(); err != nil {
 		if !errors.Is(err, http.ErrServerClosed) {
 			app.Logger.Error("Server failed", logger.WithErr(err))
 			return
 		}
 	}
+
+	wg.Wait()
 
 	if cerr := <-eChan; cerr != nil {
 		app.Logger.Error("Failed graceful shutdown", logger.WithErr(cerr))
---
+++ internal/adapters/driving/grpc/server.go
@@ -0,0 +1,81 @@
+package grpc
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"net"
+	"sync"
+
+	"github.com/shrtyk/avito-pvz-test-assignment/internal/core/ports/service"
+	pvz "github.com/shrtyk/avito-pvz-test-assignment/proto/pvz/gen"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type Server struct {
+	wg         *sync.WaitGroup
+	port       string
+	appService service.Service
+	logger     *slog.Logger
+	grpcServ   *grpc.Server
+
+	pvz.UnimplementedPVZServiceServer
+}
+
+func NewGRPCServer(
+	wg *sync.WaitGroup,
+	appService service.Service,
+	logger *slog.Logger,
+	port string,
+) *Server {
+	gs := grpc.NewServer()
+	s := &Server{
+		wg:         wg,
+		port:       port,
+		appService: appService,
+		logger:     logger,
+		grpcServ:   gs,
+	}
+
+	pvz.RegisterPVZServiceServer(s.grpcServ, s)
+	reflection.Register(gs)
+
+	return s
+}
+
+func (s *Server) MustStart() {
+	l, err := net.Listen("tcp", ":"+s.port)
+	if err != nil {
+		msg := fmt.Sprintf("failed create net.Listener: %s", err)
+		panic(msg)
+	}
+
+	s.wg.Add(1)
+	go func() {
+		defer s.wg.Done()
+		s.logger.Info("grpc server runnning", slog.String("port", s.port))
+		if err := s.grpcServ.Serve(l); err != nil {
+			msg := fmt.Sprintf("failed to start grpc server: %s", err)
+			panic(msg)
+		}
+	}()
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	done := make(chan struct{})
+	go func() {
+		s.grpcServ.GracefulStop()
+		close(done)
+	}()
+
+	select {
+	case <-ctx.Done():
+		s.logger.Warn("grpcs server graceful shutdown time out; forcing stop")
+		s.grpcServ.Stop()
+		return ctx.Err()
+	case <-done:
+		s.logger.Info("grpc server graceful shtudown complete")
+		return nil
+	}
+}
